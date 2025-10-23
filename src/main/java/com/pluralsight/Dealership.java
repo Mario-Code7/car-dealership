616766137308ea739a0ce5d90a6354431b2636bb
Mario-Code7 <marioroa71@yahoo.com>
2025-10-23 15:53:53 -0500

    added getters in Dealership

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -16,7 +16,57 @@ public class Dealership {
         this.vehicles = new ArrayList<>();
     }
 
-    public ArrayList<Vehicle> getVehicles() {
-        return vehicles;
+    public List<Vehicle> getVehiclesByPrice(double min, double max){
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max){
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color){
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max){
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+
+    }
+
+    public void getAllVehicles(){
+
+    }
+
+    public void removeVehicles(Vehicle vehicle){
+
     }
 }
